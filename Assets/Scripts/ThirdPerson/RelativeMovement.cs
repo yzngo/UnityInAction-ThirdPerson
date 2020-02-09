95009acf202688484cb8dd17454d3dd12c47c3ae
yzngo <yzngo@live.com>
2020-02-09 23:36:07 +0800

    update

--- Assets/Scripts/ThirdPerson/RelativeMovement.cs
+++ Assets/Scripts/ThirdPerson/RelativeMovement.cs
@@ -8,10 +8,18 @@ public class RelativeMovement : MonoBehaviour
     [SerializeField] private Transform target;
     public float rotSpeed = 15.0f;
     public float moveSpeed = 6.0f;
+    public float jumpSpeed = 15.0f;
+    public float gravity = -9.8f;
+    public float terminalVelocity = -10.0f;
+    public float minFall = -1.5f;
+
+    private float _vertSpeed;
     private CharacterController _charController;
+    private ControllerColliderHit _contact;
     // Start is called before the first frame update
     void Start()
     {
+        _vertSpeed = minFall;
         _charController = GetComponent<CharacterController>();
     }
 
@@ -19,7 +27,7 @@ public class RelativeMovement : MonoBehaviour
     void Update()
     {
         Vector3 movement = Vector3.zero;
-        // 解释在Page 109
+        // move around 解释在Page 109
         float horInput = Input.GetAxis("Horizontal");
         float vertInput = Input.GetAxis("Vertical");
         if(horInput != 0 || vertInput != 0) {
@@ -35,7 +43,40 @@ public class RelativeMovement : MonoBehaviour
             Quaternion direction = Quaternion.LookRotation(movement);
             transform.rotation = Quaternion.Lerp(transform.rotation, direction, rotSpeed * Time.deltaTime);
         }
+        // jump
+        bool hitGround = false;
+        RaycastHit hit;
+        if (_vertSpeed < 0 &&
+            Physics.Raycast(transform.position, Vector3.down, out hit)){
+            float check = (_charController.height + _charController.radius) / 1.9f;
+            hitGround = hit.distance <= check;
+        }
+        if (hitGround) {
+            if(Input.GetButtonDown("Jump")) {
+                _vertSpeed = jumpSpeed;
+            } else {
+                _vertSpeed = minFall;
+            }
+        } else {
+            _vertSpeed += gravity * 5 * Time.deltaTime;
+            if (_vertSpeed < terminalVelocity) {
+                _vertSpeed = terminalVelocity;
+            }
+
+            if (_charController.isGrounded) {
+                if(Vector3.Dot(movement, _contact.normal) < 0) {
+                    movement = _contact.normal * moveSpeed;
+                } else {
+                    movement += _contact.normal * moveSpeed;
+                }
+            }
+        }
+        movement.y = _vertSpeed;
         movement *= Time.deltaTime;
         _charController.Move(movement);
+    }
+
+    void OnControllerColliderHit(ControllerColliderHit hit) {
+        _contact = hit;
     }
 }
