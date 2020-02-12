18adf5a248e19ad28a4a9f3fc4aac63210bb4511
yzngo <yzngo@live.com>
2020-02-13 00:24:44 +0800

    update

---
+++ Assets/Scripts/Interactive/DoorOpenDevice.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpenDevice : MonoBehaviour
+{
+    [SerializeField] private Vector3 dPos;
+
+    private bool _open;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Operate() {
+        if (_open) {
+            Vector3 pos = transform.position - dPos;
+            transform.position = pos;
+        } else {
+            Vector3 pos = transform.position + dPos;
+            transform.position = pos;
+        }
+        _open = !_open;
+    }
+}
