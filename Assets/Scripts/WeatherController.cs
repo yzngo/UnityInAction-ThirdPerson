3f12ad02a889a860b17662cc4be4e34b7b6f4fcd
yzngo <yzngo@live.com>
2020-02-27 10:43:05 +0800

    230

---
+++ Assets/Scripts/WeatherController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeatherController : MonoBehaviour
+{
+    [SerializeField] private Material sky;
+    [SerializeField] private Light sun;
+    private float _fullIntensity;
+    private float _cloudValue = 0f;
+    private bool darker = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _fullIntensity = sun.intensity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetOvercast(_cloudValue);
+        if (darker == true) {
+            _cloudValue += .001f;
+            if (_cloudValue >= 1.0f ) {
+                darker = false;
+            }
+        } else {
+            _cloudValue -= .001f;
+            if (_cloudValue <= 0.0f ) {
+                darker = true;
+            }
+        }
+    }
+
+    private void SetOvercast(float value)
+    {
+        sky.SetFloat("_Blend", value);
+        sun.intensity = _fullIntensity - (_fullIntensity * value);
+    }
+}
