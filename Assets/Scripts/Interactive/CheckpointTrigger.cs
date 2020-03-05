ec344b728a476e160a5eb4affff13e9e4b0dca10
yzngo <yzngo@live.com>
2020-03-05 23:00:03 +0800

    251

---
+++ Assets/Scripts/Interactive/CheckpointTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointTrigger : MonoBehaviour
+{
+    public string identifier;
+    private bool _triggered;
+    void OnTriggerEnter(Collider other)
+    {
+        if (_triggered) {
+            return;
+        }
+        Managers.Weather.LogWeather(identifier);
+        _triggered = true;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/NetworkService.cs
+++ Assets/Scripts/NetworkService.cs
@@ -9,7 +9,7 @@ public class NetworkService
     private const string xmlApi = "http://api.openweathermap.org/data/2.5/weather?q=HangZhou,CN&mode=xml&APPID=5b5269ade62687a6847d28196df214bd";
     private const string jsonApi = "http://api.openweathermap.org/data/2.5/weather?q=HangZhou,CN&APPID=5b5269ade62687a6847d28196df214bd";
     private const string webImage = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1583255876005&di=2052dbe788538cf1a7fa02b266c7cf07&imgtype=0&src=http%3A%2F%2Fi0.hdslb.com%2Fbfs%2Farticle%2F19de927d6de05d9ea9d6cc6a5042f8bb0f270100.jpg";
-    private const string localApi = "http://127.0.0.1:8080/temp/hello.txt";
+    private const string localApi = "http://127.0.0.1:8080/playfab/savefile.lua";
 
     private IEnumerator CallAPI(string url, WWWForm form, Action<string> callback)
     {
