3797df1b8304ebc4698cbd69daae3906f5f8ead5
Yzngo <yzngo@live.com>
2020-03-20 18:56:14 +0800

    update

--- Assets/Scripts/UI/SettingsPopup.cs
+++ Assets/Scripts/UI/SettingsPopup.cs
@@ -38,4 +38,20 @@ public class SettingsPopup : MonoBehaviour
         Managers.Audio.PlaySound(clip: sound);
     }
     public void OnSoundValue(float volume) => Managers.Audio.soundVolume = volume;
+
+    public void OnPlayMusic(int selector)
+    {
+        Managers.Audio.PlaySound(clip: sound);
+        switch (selector) {
+            case 1:
+                Managers.Audio.PlayIntroMusic();
+                break;
+            case 2:
+                Managers.Audio.PlayLevelMusic();
+                break;
+            default:
+                Managers.Audio.StopMusic();
+                break;
+        }
+    }
 }
